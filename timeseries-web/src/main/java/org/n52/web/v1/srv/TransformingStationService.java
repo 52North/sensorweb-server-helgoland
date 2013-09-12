7f47984586e248cf1bf62eaa0273dbb262fc3c76
Henning Bredel <h.bredel@52north.org>
2013-09-12 10:22:10 +0200

    add station transformation also for timeseries (including stations)

--- timeseries-web/src/main/java/org/n52/web/v1/srv/TransformingStationService.java
+++ timeseries-web/src/main/java/org/n52/web/v1/srv/TransformingStationService.java
@@ -24,26 +24,14 @@
 
 package org.n52.web.v1.srv;
 
-import static org.n52.io.crs.CRSUtils.DEFAULT_CRS;
-import static org.n52.io.crs.CRSUtils.createEpsgStrictAxisOrder;
-
 import org.n52.io.IoParameters;
-import org.n52.io.crs.CRSUtils;
 import org.n52.io.v1.data.StationOutput;
-import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.operation.TransformException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.vividsolutions.jts.geom.Point;
 
 /**
  * Composes a {@link ParameterService} for {@link StationOutput}s to transform geometries to requested spatial
  * reference system.
  */
-public class TransformingStationService implements ParameterService<StationOutput> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(TransformingStationService.class);
+public class TransformingStationService extends TransformationService implements ParameterService<StationOutput> {
 
     private ParameterService<StationOutput> composedService;
 
@@ -65,7 +53,8 @@ public class TransformingStationService implements ParameterService<StationOutpu
 
     @Override
     public StationOutput[] getParameters(String[] items) {
-        return composedService.getParameters(items);
+        StationOutput[] stations = composedService.getParameters(items);
+        return transformStations(IoParameters.createDefaults(), stations);
     }
 
     @Override
@@ -76,38 +65,16 @@ public class TransformingStationService implements ParameterService<StationOutpu
 
     @Override
     public StationOutput getParameter(String item) {
-        return composedService.getParameter(item);
+        StationOutput station = composedService.getParameter(item);
+        transformInline(station, IoParameters.createDefaults());
+        return station;
     }
 
     @Override
     public StationOutput getParameter(String item, IoParameters query) {
         StationOutput station = composedService.getParameter(item, query);
-        transformInline(station, query.getCrs());
+        transformInline(station, query);
         return station;
     }
-
-    private StationOutput[] transformStations(IoParameters query, StationOutput[] stations) {
-        for (StationOutput stationOutput : stations) {
-            transformInline(stationOutput, query.getCrs());
-        }
-        return stations;
-    }
-
-    private void transformInline(StationOutput stationOutput, String targetCrs) {
-        if (DEFAULT_CRS.equals(targetCrs)) {
-            return; // no need to transform
-        }
-        try {
-            CRSUtils crsUtils = createEpsgStrictAxisOrder();
-            Point point = crsUtils.convertToPointFrom(stationOutput.getGeometry());
-            stationOutput.setGeometry(crsUtils.convertToGeojsonFrom(point, targetCrs));
-        }
-        catch (TransformException e) {
-            LOGGER.warn("Could not transform to requested CRS: {}", targetCrs, e);
-        }
-        catch (FactoryException e) {
-            LOGGER.error("Could not create CRS {}.", targetCrs, e);
-        }
-    }
-
+    
 }
