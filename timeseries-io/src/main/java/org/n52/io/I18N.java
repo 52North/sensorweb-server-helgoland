8df1ba6289d85297e7778abd3a897b6bfa3f1449
Henning Bredel <h.bredel@52north.org>
2014-01-07 13:46:14 +0100

    use customized I18N control class to read localization messages stored in UTF-8

--- timeseries-io/src/main/java/org/n52/io/I18N.java
+++ timeseries-io/src/main/java/org/n52/io/I18N.java
@@ -21,12 +21,23 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
  * visit the Free Software Foundation web page, http://www.fsf.org.
  */
+
 package org.n52.io;
 
 import static java.util.ResourceBundle.getBundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Locale;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
+import java.util.ResourceBundle.Control;
 
 public final class I18N {
 
@@ -59,7 +70,7 @@ public final class I18N {
 
     public static I18N getMessageLocalizer(String languageCode) {
         Locale locale = createLocate(languageCode);
-        return new I18N(getBundle(MESSAGES, locale), locale);
+        return new I18N(getBundle(MESSAGES, locale, new UTF8Control()), locale);
     }
 
     private static Locale createLocate(String language) {
@@ -72,11 +83,89 @@ public final class I18N {
         }
         if (localeParts.length == 1) {
             return new Locale(localeParts[0]);
-        } else if (localeParts.length == 2){
+        }
+        else if (localeParts.length == 2) {
             return new Locale(localeParts[0], localeParts[1]);
-        } else {
+        }
+        else {
             return new Locale(localeParts[0], localeParts[1], localeParts[2]);
-        } 
+        }
+    }
+
+    /**
+     * Customized {@link Control} to enable correct bundle reading in UTF-8.
+     */
+    private static class UTF8Control extends Control {
+        public ResourceBundle newBundle(String baseName, Locale locale, String format,
+                                        ClassLoader loader, boolean reload)
+                throws IllegalAccessException, InstantiationException, IOException {
+            String bundleName = toBundleName(baseName, locale);
+            ResourceBundle bundle = null;
+            if (format.equals("java.class")) {
+                try {
+                    @SuppressWarnings("unchecked")
+                    Class< ? extends ResourceBundle> bundleClass = (Class< ? extends ResourceBundle>) loader.loadClass(bundleName);
+
+                    // If the class isn't a ResourceBundle subclass, throw a
+                    // ClassCastException.
+                    if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
+                        bundle = bundleClass.newInstance();
+                    }
+                    else {
+                        throw new ClassCastException(bundleClass.getName()
+                                + " cannot be cast to ResourceBundle");
+                    }
+                }
+                catch (ClassNotFoundException e) {
+                }
+            }
+            else if (format.equals("java.properties")) {
+                final String resourceName = toResourceName(bundleName, "properties");
+                final ClassLoader classLoader = loader;
+                final boolean reloadFlag = reload;
+                InputStream stream = null;
+                try {
+                    stream = AccessController.doPrivileged(
+                            new PrivilegedExceptionAction<InputStream>() {
+                                public InputStream run() throws IOException {
+                                    InputStream is = null;
+                                    if (reloadFlag) {
+                                        URL url = classLoader.getResource(resourceName);
+                                        if (url != null) {
+                                            URLConnection connection = url.openConnection();
+                                            if (connection != null) {
+                                                // Disable caches to get fresh data for
+                                                // reloading.
+                                                connection.setUseCaches(false);
+                                                is = connection.getInputStream();
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        is = classLoader.getResourceAsStream(resourceName);
+                                    }
+                                    return is;
+                                }
+                            });
+                }
+                catch (PrivilegedActionException e) {
+                    throw (IOException) e.getException();
+                }
+                if (stream != null) {
+                    try {
+                        InputStreamReader utf8StreamReader = new InputStreamReader(stream, "UTF-8");
+                        bundle = new PropertyResourceBundle(utf8StreamReader);
+                    }
+                    finally {
+                        stream.close();
+                    }
+                }
+            }
+            else {
+                throw new IllegalArgumentException("unknown format: " + format);
+            }
+            return bundle;
+        }
     }
 
 }
