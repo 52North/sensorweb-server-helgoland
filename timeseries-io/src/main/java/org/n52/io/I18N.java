a2bcf2279a2cf7e8338fa91ee8c501ef00124757
Henning Bredel <h.bredel@52north.org>
2014-01-15 10:31:02 +0100

    make newBundle implementation conformant to JavaDoc example

--- timeseries-io/src/main/java/org/n52/io/I18N.java
+++ timeseries-io/src/main/java/org/n52/io/I18N.java
@@ -31,9 +31,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.security.AccessController;
-import java.security.PrivilegedActionException;
-import java.security.PrivilegedExceptionAction;
+import java.util.List;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -93,63 +91,51 @@ public final class I18N {
     }
 
     /**
-     * Customized {@link Control} to enable correct bundle reading in UTF-8.
+     * Overrides {@link Control#newBundle(String, Locale, String, ClassLoader, boolean)} as given in
+     * {@link Control}'s JavaDoc example to handle UTF-8 localization bundles.
      */
     private static class UTF8Control extends Control {
+        
+        @Override
+        public List<String> getFormats(String baseName) {
+            return FORMAT_PROPERTIES;
+        }
+
+        /*
+         * Implementation taken from Control JavaDoc example.
+         * 
+         * (non-Javadoc)
+         * 
+         * @see java.util.ResourceBundle.Control#newBundle(java.lang.String, java.util.Locale,
+         * java.lang.String, java.lang.ClassLoader, boolean)
+         */
         public ResourceBundle newBundle(String baseName, Locale locale, String format,
                                         ClassLoader loader, boolean reload)
                 throws IllegalAccessException, InstantiationException, IOException {
-            String bundleName = toBundleName(baseName, locale);
-            ResourceBundle bundle = null;
-            if (format.equals("java.class")) {
-                try {
-                    @SuppressWarnings("unchecked")
-                    Class< ? extends ResourceBundle> bundleClass = (Class< ? extends ResourceBundle>) loader.loadClass(bundleName);
-
-                    // If the class isn't a ResourceBundle subclass, throw a
-                    // ClassCastException.
-                    if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
-                        bundle = bundleClass.newInstance();
-                    }
-                    else {
-                        throw new ClassCastException(bundleClass.getName()
-                                + " cannot be cast to ResourceBundle");
-                    }
-                }
-                catch (ClassNotFoundException e) {
-                }
+            if (baseName == null || locale == null || format == null || loader == null) {
+                throw new NullPointerException();
             }
-            else if (format.equals("java.properties")) {
+            ResourceBundle bundle = null;
+            if (format.equals("java.properties")) {
+                String bundleName = toBundleName(baseName, locale);
                 final String resourceName = toResourceName(bundleName, "properties");
                 final ClassLoader classLoader = loader;
                 final boolean reloadFlag = reload;
                 InputStream stream = null;
-                try {
-                    stream = AccessController.doPrivileged(
-                            new PrivilegedExceptionAction<InputStream>() {
-                                public InputStream run() throws IOException {
-                                    InputStream is = null;
-                                    if (reloadFlag) {
-                                        URL url = classLoader.getResource(resourceName);
-                                        if (url != null) {
-                                            URLConnection connection = url.openConnection();
-                                            if (connection != null) {
-                                                // Disable caches to get fresh data for
-                                                // reloading.
-                                                connection.setUseCaches(false);
-                                                is = connection.getInputStream();
-                                            }
-                                        }
-                                    }
-                                    else {
-                                        is = classLoader.getResourceAsStream(resourceName);
-                                    }
-                                    return is;
-                                }
-                            });
+                if (reloadFlag) {
+                    URL url = classLoader.getResource(resourceName);
+                    if (url != null) {
+                        URLConnection connection = url.openConnection();
+                        if (connection != null) {
+                            // Disable caches to get fresh data for
+                            // reloading.
+                            connection.setUseCaches(false);
+                            stream = connection.getInputStream();
+                        }
+                    }
                 }
-                catch (PrivilegedActionException e) {
-                    throw (IOException) e.getException();
+                else {
+                    stream = classLoader.getResourceAsStream(resourceName);
                 }
                 if (stream != null) {
                     try {
@@ -162,7 +148,7 @@ public final class I18N {
                 }
             }
             else {
-                throw new IllegalArgumentException("unknown format: " + format);
+                throw new IllegalArgumentException("Only java.properties format allowed! Was: " + format);
             }
             return bundle;
         }
