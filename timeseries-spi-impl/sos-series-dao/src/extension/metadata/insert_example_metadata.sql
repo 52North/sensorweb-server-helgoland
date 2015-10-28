e4f9e960b7f931560a647421d0bf213241cadeac
Henning Bredel <bredel@52north.org>
2015-10-28 12:16:44 +0100

    init db metadata access

---
+++ timeseries-spi-impl/sos-series-dao/src/extension/metadata/insert_example_metadata.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO series_metadata VALUES (1, 'integrationPeriod', 'string', 'PT1M') ;
+INSERT INTO series_metadata VALUES (1, 'booleanMetadataField', 'boolean','false');
+INSERT INTO series_metadata VALUES (1, 'numericMetadataField', 'double', '42.17');
+INSERT INTO series_metadata VALUES (1, 'timeseries_copyright', 'string', '(C) 2015');
---
+++ timeseries-spi-impl/sos-series-dao/src/main/java/org/n52/io/extension/MetadataExtension.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2013-2015 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as publishedby the Free
+ * Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of the
+ * following licenses, the combination of the program with the linked library is
+ * not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed under
+ * the aforementioned licenses, is permitted by the copyright holders if the
+ * distribution is compliant with both the GNU General Public License version 2
+ * and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+package org.n52.io.extension;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.n52.io.request.IoParameters;
+import org.n52.io.response.ParameterOutput;
+import org.n52.sensorweb.spi.ResultTimeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetadataExtension extends MetadataExtension<ParameterOutput> {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(MetadataExtension.class);
+
+    private static final String EXTENSION_NAME = "databaseMetadata";
+
+
+    public MetadataExtension() {
+        this(EXTENSION_NAME);
+    }
+
+    public MetadataExtension(String name) {
+        super(name);
+    }
+    
+    @Override
+    public Object getExtras(ParameterOutput output, IoParameters parameters) {
+        
+        // TODO read from db
+        
+        return null;
+    }
+
+}
