493b1e39b7affdfaf86a67c0fdf21825109efdf5
Henning Bredel <bredel@52north.org>
2015-12-01 09:49:46 +0100

    license header

--- timeseries-spi-impl/sos-series-dao/src/main/java/org/n52/series/api/v1/db/da/beans/ServiceInfo.java
+++ timeseries-spi-impl/sos-series-dao/src/main/java/org/n52/series/api/v1/db/da/beans/ServiceInfo.java
@@ -10,20 +10,20 @@
  * following licenses, the combination of the program with the linked library is
  * not considered a "derivative work" of the program:
  *
- * - Apache License, version 2.0 - Apache Software License, version 1.0 - GNU
- * Lesser General Public License, version 3 - Mozilla Public License, versions
- * 1.0, 1.1 and 2.0 - Common Development and Distribution License (CDDL),
- * version 1.0
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
  *
- * Therefore the distribution of the program linked with libraries licensed
- * under the aforementioned licenses, is permitted by the copyright holders if
- * the distribution is compliant with both the GNU General Public License
- * version 2 and the aforementioned licenses.
+ * Therefore the distribution of the program linked with libraries licensed under
+ * the aforementioned licenses, is permitted by the copyright holders if the
+ * distribution is compliant with both the GNU General Public License version 2
+ * and the aforementioned licenses.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  */
 package org.n52.series.api.v1.db.da.beans;
 
