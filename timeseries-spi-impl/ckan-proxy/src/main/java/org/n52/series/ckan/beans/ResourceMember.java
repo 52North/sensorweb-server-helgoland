ce6cc01116c36ed187529a0ac992c83f706471cd
Henning Bredel <bredel@52north.org>
2015-12-14 07:04:24 +0100

    remove type

--- timeseries-spi-impl/ckan-proxy/src/main/java/org/n52/series/ckan/beans/ResourceMember.java
+++ timeseries-spi-impl/ckan-proxy/src/main/java/org/n52/series/ckan/beans/ResourceMember.java
@@ -1,19 +1,17 @@
 package org.n52.series.ckan.beans;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class ResourceMember {
     
-    private enum ResourceMemberType {
-        PLATFORMS,
-        OBSERVATIONS
-    }
-    
     private String id;
     
-    private ResourceMemberType resourceType;
-    
     private int headerRows;
+    
+    private String resourceType;
     
     private List<ResourceField> resourceFields;
 
@@ -26,11 +24,11 @@ public class ResourceMember {
     }
 
     public String getResourceType() {
-        return resourceType.toString().toLowerCase();
+        return resourceType;
     }
 
     public void setResourceType(String resourceType) {
-        this.resourceType = ResourceMemberType.valueOf(resourceType.toUpperCase());
+        this.resourceType = resourceType;
     }
 
     public int getHeaderRows() {
@@ -42,12 +40,61 @@ public class ResourceMember {
     }
 
     public List<ResourceField> getResourceFields() {
-        return resourceFields;
+        return Collections.unmodifiableList(resourceFields);
+    }
+    
+    public ResourceField getField(String fieldId) {
+        for (ResourceField field : resourceFields) {
+            if (field.getFieldId().equalsIgnoreCase(fieldId)) {
+                return field;
+            }
+        }
+        return null;
+    }
+    
+    public boolean containsField(String fieldId) {
+        for (ResourceField field : resourceFields) {
+            if (field.getFieldId().equalsIgnoreCase(fieldId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public List<String> getColumnHeaders() {
+        List<String> headers = new ArrayList<>();
+        for (ResourceField field : getResourceFields()) {
+            headers.add(field.getShortName());
+        }
+        return headers;
     }
 
     public void setResourceFields(List<ResourceField> resourceFields) {
         this.resourceFields = resourceFields;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ResourceMember other = (ResourceMember) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
     
