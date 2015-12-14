9f11a46c166bf17ebee3218f36066f353f1b9cee
Henning Bredel <bredel@52north.org>
2015-12-14 07:04:45 +0100

    some table handson

--- timeseries-spi-impl/ckan-proxy/src/main/java/org/n52/series/ckan/table/TableMerger.java
+++ timeseries-spi-impl/ckan-proxy/src/main/java/org/n52/series/ckan/table/TableMerger.java
@@ -1,5 +1,108 @@
-package org.n52.series.ckan.util;
+package org.n52.series.ckan.table;
 
-public class TableMerger {
+import com.google.common.collect.HashBasedTable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.n52.series.ckan.beans.CsvObservationsCollection;
+import org.n52.series.ckan.beans.DataFile;
+import org.n52.series.ckan.beans.ResourceMember;
+
+
+class TableMerger {
     
+    private final HashBasedTable<String,String,String> table;
+    
+    private final CsvObservationsCollection observationCollection;
+
+    public static HashBasedTable<String,String,String> merge(ObservationTable table) {
+        this.table = HashBasedTable.create();
+    }
+    
+    private void joinResourceTables(CsvObservationsCollection observationCollection) {
+        Map<ResourceMember, DataFile> dataCollection = observationCollection.getDataCollection();
+        Set<String> joinFields = getJoinFields(dataCollection.keySet());
+        for (Map.Entry<ResourceMember, DataFile> entry : dataCollection.entrySet()) {
+            
+        }
+    }
+
+    private Set<String> getJoinFields(Set<ResourceMember> members) {
+        List<String> allColumns = new ArrayList<>();
+        ColumnCounter counter = new ColumnCounter();
+        for (ResourceMember member : members) {
+            final List<String> columnHeaders = member.getColumnHeaders();
+            counter.updateWith(columnHeaders);
+            allColumns.addAll(columnHeaders);
+        }
+        
+        Set<String> joinColumns = new LinkedHashSet<>();
+        for (String column : allColumns) {
+            if (counter.isJoinColumn(column)) {
+                joinColumns.add(column);
+            }
+        }
+        return joinColumns;
+    }
+    
+    private class ColumnCounter {
+        private final Map<String, ColumnCount> counts = new HashMap<>();
+        void updateWith(List<String> columns) {
+            for (String column : columns) {
+                if (counts.containsKey(column)) {
+                    counts.get(column).count++;
+                } else {
+                    counts.put(column, new ColumnCount());
+                }
+            }
+        }
+        boolean isJoinColumn(String column) {
+            return counts.get(column).count > 1;
+        }
+    }
+    
+    private class ColumnCount {
+        private int count;
+        public ColumnCount() {
+            count++;
+        }
+    }
+    
+    // TODO
+    public void printEachMemberIndividually(CsvObservationsCollection collection) {
+        StringBuilder horizontalLine = new StringBuilder("+-");
+        StringBuilder headerLine = new StringBuilder();
+        StringBuilder format = new StringBuilder("| ");
+        for (Map.Entry<ResourceMember, DataFile> entry : collection.getDataCollection().entrySet()) {
+            ResourceMember member = entry.getKey();
+            List<String> columnsHeaders = member.getColumnHeaders();
+            int[] colunmLengths = new int[columnsHeaders.size()];
+            for (int i = 0 ; i < colunmLengths.length; i++) {
+                final String header = columnsHeaders.get(i);
+                final int length = header.length();
+                int width = Math.round(length + 0.2f * length);
+                final String columnFormat = "%-" + width + "s | ";
+                format.append(columnFormat);
+                
+                for (int j = 0 ; j < width ; j++) {
+                    horizontalLine.append("-");
+                }
+                horizontalLine.append("-+");
+                
+            }
+            
+            format.append(" |%n");
+            horizontalLine.append("-+%n");
+            String line = horizontalLine.toString();
+            headerLine
+                    .append(line)
+                    .append(String.format(format.toString(), columnsHeaders.toArray()))
+                    .append(line);
+            System.out.format(headerLine.toString());
+        }
+        
+    }
 }
--- timeseries-spi-impl/ckan-proxy/src/test/java/org/n52/series/ckan/da/CkanHarvestingServiceTest.java
+++ timeseries-spi-impl/ckan-proxy/src/test/java/org/n52/series/ckan/da/CkanHarvestingServiceTest.java
@@ -1,56 +1,78 @@
 package org.n52.series.ckan.da;
 
 import org.n52.series.ckan.cache.InMemoryCkanMetadataCache;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.trentorise.opendata.jackan.CkanClient;
 import eu.trentorise.opendata.jackan.CkanQuery;
+import eu.trentorise.opendata.jackan.model.CkanDataset;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Map;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.n52.series.ckan.beans.CsvObservationsCollection;
+import org.n52.series.ckan.beans.DataFile;
+import org.n52.series.ckan.beans.ResourceMember;
+import org.n52.series.ckan.table.ObservationTable;
+import org.n52.series.ckan.cache.InMemoryCkanDataCache;
+import org.n52.series.ckan.cache.InMemoryCkanDataCache.Entry;
 import org.n52.series.ckan.util.ResourceClient;
 
-public class CkanHarvesterTest {
+public class CkanHarvestingServiceTest {
     
     private final ObjectMapper om = new ObjectMapper();
     
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
     
-    private InMemoryCkanMetadataCache ckanCache;
+    private InMemoryCkanMetadataCache ckanMetadataCache;
     
-    private CkanHarvester ckanHarvester;
+    private InMemoryCkanDataCache ckanDataCache;
+    
+    private CkanHarvestingService ckanHarvester;
     
     @Before
     public void setUp() throws URISyntaxException {
-        ckanCache = new InMemoryCkanMetadataCache();
-        ckanHarvester = new CkanHarvester();
-//        ckanHarvester.setResourceClient(new SeamResourceClient());
-        ckanHarvester.setResourceClient(new ResourceClient());
-        ckanHarvester.setCkanClient(new CkanClient("https://ckan.colabis.de"));
-        ckanHarvester.setResourceDownloadBaseFolder(testFolder.getRoot().toURI().toString());
-        ckanHarvester.setDatasetCache(ckanCache);
+        ckanMetadataCache = new InMemoryCkanMetadataCache();
+        ckanDataCache = new InMemoryCkanDataCache();
+        ckanHarvester = new CkanHarvestingService();
         
-        MatcherAssert.assertThat(ckanCache.size(), CoreMatchers.is(0));
+        ckanHarvester.setResourceClient(new ResourceClient()); // TODO load locally from seam
+        ckanHarvester.setCkanClient(new CkanClient("https://ckan.colabis.de")); // TODO load locally from seam
+        ckanHarvester.setResourceDownloadBaseFolder(testFolder.getRoot().toURI().toString());
+        ckanHarvester.setMetadataCache(ckanMetadataCache);
+        ckanHarvester.setDataCache(ckanDataCache);
+        
+        MatcherAssert.assertThat(ckanMetadataCache.size(), CoreMatchers.is(0));
         CkanQuery query = CkanQuery.filter().byTagNames("DWD");
         ckanHarvester.harvestDatasets(query);
     }
     
     @Test
     public void harvestDatasets() {
-        MatcherAssert.assertThat(ckanCache.size(), CoreMatchers.is(4));
+        MatcherAssert.assertThat(ckanMetadataCache.size(), CoreMatchers.is(4));
     }
     
     @Test
     public void harvestResources() throws IOException {
         MatcherAssert.assertThat(testFolder.getRoot().list().length, CoreMatchers.is(0));
         ckanHarvester.harvestResources();
+        
+        Entry<CkanDataset, CsvObservationsCollection> entry = ckanDataCache.getCollections().iterator().next();
+        Map<ResourceMember, DataFile> dataCollection = entry.getData().getDataCollection();
+        Map.Entry<ResourceMember, DataFile> data = dataCollection.entrySet().iterator().next();
+        ObservationTable observationTable = new ObservationTable(data.getKey(), data.getValue());
+        observationTable.readIntoMemory();
+        
+//        for (Entry<CkanDataset, CsvObservationsCollection> entry : ckanDataCache.getCollections()) {
+//            TableMerger tableMerger = new TableMerger(entry.getData());
+//            System.out.println("#### DATASET: " + entry.getDataset().getId());
+//            tableMerger.printEachMemberIndividually(entry.getData());
+//        }
         
         // TODO
     }
