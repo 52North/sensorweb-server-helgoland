b233eb314fa28e0fd68a39e7d248556e269d25c3
Henning Bredel <bredel@52north.org>
2016-01-04 16:15:23 +0100

    store field index to support multiple phenomena in one row

--- timeseries-spi-impl/ckan-proxy/src/main/java/org/n52/series/ckan/sos/Phenomenon.java
+++ timeseries-spi-impl/ckan-proxy/src/main/java/org/n52/series/ckan/sos/Phenomenon.java
@@ -6,18 +6,21 @@ public class Phenomenon {
     
     private final String id;
     
+    private final int fieldIdx;
+    
     private final String label;
     
     private final String uom;
 
-    public Phenomenon(String id, String label) {
-        this(id, label, null);
+    public Phenomenon(String id, String label, int fieldIdx) {
+        this(id, label, fieldIdx, null);
     }
 
-    public Phenomenon(String id, String label, String uom) {
+    public Phenomenon(String id, String label, int fieldIdx, String uom) {
         this.id = id;
         this.uom = uom;
         this.label = label;
+        this.fieldIdx = fieldIdx;
     }
 
     public String getId() {
@@ -32,12 +35,17 @@ public class Phenomenon {
         return uom;
     }
 
+    public int getFieldIdx() {
+        return fieldIdx;
+    }
+
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 19 * hash + Objects.hashCode(this.id);
-        hash = 19 * hash + Objects.hashCode(this.label);
-        hash = 19 * hash + Objects.hashCode(this.uom);
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + this.fieldIdx;
+        hash = 37 * hash + Objects.hashCode(this.label);
+        hash = 37 * hash + Objects.hashCode(this.uom);
         return hash;
     }
 
@@ -53,6 +61,9 @@ public class Phenomenon {
         if (!Objects.equals(this.id, other.id)) {
             return false;
         }
+        if (this.fieldIdx != other.fieldIdx) {
+            return false;
+        }
         if (!Objects.equals(this.label, other.label)) {
             return false;
         }
@@ -61,5 +72,7 @@ public class Phenomenon {
         }
         return true;
     }
+
+    
     
 }
