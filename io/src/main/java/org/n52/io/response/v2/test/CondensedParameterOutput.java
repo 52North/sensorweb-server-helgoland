f4f0f1687c29c8ec1cafaf33429630bb5a70f155
Henning Bredel <bredel@52north.org>
2016-05-03 15:46:29 +0200

    simplify access services

---
+++ io/src/main/java/org/n52/io/response/v2/test/CondensedParameterOutput.java
@@ -0,0 +1,15 @@
+package org.n52.io.response.v2.test;
+
+/**
+ * TODO: JavaDoc
+ *
+ * @author <a href="mailto:h.bredel@52north.org">Henning Bredel</a>
+ */
+public interface CondensedParameterOutput {
+
+    String getId();
+
+    String getHref();
+
+    String getLabel();
+}
