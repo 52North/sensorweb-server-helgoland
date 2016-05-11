2cd5bd04d864e617d1c02ab8c5392bd9cbc6047d
Henning Bredel <bredel@52north.org>
2016-05-11 17:43:03 +0200

    detect platform type from series

--- spi-impl/series-dao/src/main/java/org/n52/series/db/da/beans/ext/PlatformEntity.java
+++ spi-impl/series-dao/src/main/java/org/n52/series/db/da/beans/ext/PlatformEntity.java
@@ -1,33 +1,63 @@
 package org.n52.series.db.da.beans.ext;
 
-import java.util.List;
+import java.util.Set;
 import org.n52.io.response.v1.ext.PlatformType;
 import org.n52.series.db.da.beans.DescribableEntity;
+import org.n52.series.db.da.beans.FeatureEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO: JavaDoc
  *
  * @author <a href="mailto:h.bredel@52north.org">Henning Bredel</a>
  */
-public class PlatformEntity extends DescribableEntity<I18nPlatformEntity> {
+public class PlatformEntity extends DescribableEntity {
 
-    private String platformType = PlatformType.STATIONARY_INSITU.getTypeName();
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlatformEntity.class);
 
-    private List<AbstractSeriesEntity> series;
+    private Set<AbstractSeriesEntity> series;
 
-    public String getPlatformType() {
-        return platformType;
-    }
+    private PlatformType platformType;
 
     public void setPlatformType(String platformType) {
-        this.platformType = platformType;
+        this.platformType = PlatformType.isKnownType(platformType)
+                ? PlatformType.toInstance(platformType)
+                : null;
     }
 
-    public List<AbstractSeriesEntity> getSeries() {
+    public PlatformType getPlatformType() {
+        PlatformType type = platformType;
+        if (series == null) {
+            return PlatformType.STATIONARY_INSITU;
+        }
+        for (AbstractSeriesEntity entity : series) {
+            final FeatureEntity feature = entity.getFeature();
+            final String concept = feature.getFeatureConcept();
+            if (!PlatformType.isKnownType(concept)) {
+                LOGGER.warn("unknown feature concept for feature '{}': {}", feature.getPkid(), concept);
+                continue;
+            }
+            // TODO log warning when not consistent?!
+//            PlatformType tmp = PlatformType.toInstance(concept);
+//            if (type != null && tmp != type) {
+//                LOGGER.warn("Different platform type referenced: {} vs. {}", tmp, type);
+//            } else {
+//                type = tmp;
+//            }
+            type = PlatformType.toInstance(concept);
+            break;
+        }
+        return type == null
+                ? PlatformType.STATIONARY_INSITU
+                : type;
+    }
+
+    public Set<AbstractSeriesEntity> getSeries() {
         return series;
     }
 
-    public void setSeries(List<AbstractSeriesEntity> series) {
+    public void setSeries(Set<AbstractSeriesEntity> series) {
         this.series = series;
     }
 
