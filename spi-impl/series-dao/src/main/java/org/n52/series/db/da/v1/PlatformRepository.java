1fe7dc3efcf82e28fc82b86fb9819b21619418bb
Henning Bredel <bredel@52north.org>
2016-05-25 12:33:26 +0200

    change some log entries

--- spi-impl/series-dao/src/main/java/org/n52/series/db/da/v1/PlatformRepository.java
+++ spi-impl/series-dao/src/main/java/org/n52/series/db/da/v1/PlatformRepository.java
@@ -143,7 +143,7 @@ public class PlatformRepository extends ExtendedSessionAwareRepository implement
     }
 
     private List<PlatformEntity> getAllStationaryRemote(DbQuery parameters, Session session) throws DataAccessException {
-        LOGGER.warn("not implemented yet.");
+        LOGGER.warn("getAllStationaryRemote() not implemented yet.");
         return Collections.emptyList();
     }
 
@@ -189,13 +189,12 @@ public class PlatformRepository extends ExtendedSessionAwareRepository implement
     }
 
     private List<PlatformEntity> getAllMobileInsitu(DbQuery parameters, Session session) throws DataAccessException {
-        LOGGER.warn("not implemented yet.");
         PlatformDao dao = new PlatformDao(session);
         return dao.getAllInstances(parameters);
     }
 
     private List<PlatformEntity> getAllMobileRemote(DbQuery parameters, Session session) throws DataAccessException {
-        LOGGER.warn("not implemented yet.");
+        LOGGER.warn("getAllMobileRemote() not implemented yet.");
         return Collections.emptyList();
     }
 
