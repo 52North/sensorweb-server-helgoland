6fef1b17a1ec8c8a2c6c914de405943f7d1a448a
Henning Bredel <bredel@52north.org>
2016-05-31 11:21:37 +0200

    add geometry to data output

--- io/src/main/java/org/n52/io/response/series/SeriesData.java
+++ io/src/main/java/org/n52/io/response/series/SeriesData.java
@@ -2,8 +2,23 @@ package org.n52.io.response.series;
 
 import java.io.Serializable;
 
+import org.n52.io.geojson.GeoJSONGeometrySerializer;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vividsolutions.jts.geom.Geometry;
+
 public class SeriesData implements Serializable {
 
     private static final long serialVersionUID = 3119211667773416585L;
 
+    private Geometry geometry;
+    
+    @JsonSerialize(using = GeoJSONGeometrySerializer.class)
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
 }
