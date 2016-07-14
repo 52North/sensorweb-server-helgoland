8827f8287bbf7f3b022ebf6b654c9b30677c2569
Henning Bredel <bredel@52north.org>
2016-07-14 17:18:49 +0200

    merged funcationality from orphaned ExtendedSessionAwareRepository

--- spi-impl/series-dao/src/main/java/org/n52/series/db/SessionAwareRepository.java
+++ spi-impl/series-dao/src/main/java/org/n52/series/db/SessionAwareRepository.java
@@ -26,15 +26,32 @@
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  */
-package org.n52.series.db.da;
+package org.n52.series.db;
 
-import org.n52.series.db.da.dao.v1.AbstractDbQuery;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.n52.series.db.dao.AbstractDbQuery;
 import org.hibernate.Session;
 import org.n52.io.crs.CRSUtils;
 import org.n52.io.request.IoParameters;
-import org.n52.io.response.ServiceOutput;
-import org.n52.series.db.da.beans.DescribableEntity;
-import org.n52.series.db.da.beans.ServiceInfo;
+import org.n52.io.response.ParameterOutput;
+import org.n52.io.response.series.SeriesParameters;
+import org.n52.io.response.v1.CategoryOutput;
+import org.n52.io.response.v1.FeatureOutput;
+import org.n52.io.response.v1.OfferingOutput;
+import org.n52.io.response.v1.PhenomenonOutput;
+import org.n52.io.response.v1.ProcedureOutput;
+import org.n52.io.response.v1.ServiceOutput;
+import org.n52.io.response.v1.ext.PlatformOutput;
+import org.n52.series.db.beans.DatasetEntity;
+import org.n52.series.db.beans.DescribableEntity;
+import org.n52.series.db.beans.PlatformEntity;
+import org.n52.series.db.beans.ServiceInfo;
+import org.n52.series.db.beans.MeasurementDatasetEntity;
+import org.n52.series.db.dao.DbQuery;
+import org.n52.series.db.da.ServiceRepository;
+import org.n52.web.ctrl.UrlHelper;
 import org.n52.web.exception.BadRequestException;
 import org.n52.web.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
@@ -53,11 +70,21 @@ public abstract class SessionAwareRepository<DBQ extends AbstractDbQuery> {
     private HibernateSessionStore sessionStore;
 
     @Autowired
+    private ServiceRepository serviceRepository;
+
+    @Autowired
     private ServiceInfo serviceInfo;
 
-    protected abstract ServiceOutput getServiceOutput() throws DataAccessException;
+    protected UrlHelper urHelper = new UrlHelper();
 
-    protected abstract DBQ getDbQuery(IoParameters parameters);
+    protected ServiceOutput getServiceOutput() throws DataAccessException {
+        List<ServiceOutput> all = serviceRepository.getAllCondensed(null);
+        return all.get(0); // only this service available
+    }
+
+    protected DbQuery getDbQuery(IoParameters parameters) {
+        return DbQuery.createFrom(parameters);
+    }
 
     public ServiceInfo getServiceInfo() {
         return serviceInfo;
@@ -113,5 +140,115 @@ public abstract class SessionAwareRepository<DBQ extends AbstractDbQuery> {
     public void setDatabaseSrid(String databaseSrid) {
         this.databaseSrid = databaseSrid;
     }
+
+    protected Map<String, SeriesParameters> createTimeseriesList(List<MeasurementDatasetEntity> series, DbQuery parameters) throws DataAccessException {
+        Map<String, SeriesParameters> timeseriesOutputs = new HashMap<>();
+        for (MeasurementDatasetEntity timeseries : series) {
+            if (!timeseries.getProcedure().isReference()) {
+                String timeseriesId = timeseries.getPkid().toString();
+                timeseriesOutputs.put(timeseriesId, createTimeseriesOutput(timeseries, parameters));
+            }
+        }
+        return timeseriesOutputs;
+    }
+
+    protected SeriesParameters createTimeseriesOutput(MeasurementDatasetEntity timeseries, DbQuery parameters) throws DataAccessException {
+        SeriesParameters timeseriesOutput = new SeriesParameters();
+        timeseriesOutput.setService(getCondensedService(parameters));
+        timeseriesOutput.setOffering(getCondensedOffering(timeseries.getProcedure(), parameters));
+        timeseriesOutput.setProcedure(getCondensedProcedure(timeseries.getProcedure(), parameters));
+        timeseriesOutput.setPhenomenon(getCondensedPhenomenon(timeseries.getPhenomenon(), parameters));
+        timeseriesOutput.setFeature(getCondensedFeature(timeseries.getFeature(), parameters));
+        timeseriesOutput.setCategory(getCondensedCategory(timeseries.getCategory(), parameters));
+        return timeseriesOutput;
+    }
+
+    protected SeriesParameters createSeriesParameters(DatasetEntity series, DbQuery parameters) throws DataAccessException {
+        SeriesParameters seriesParameter = new SeriesParameters();
+        seriesParameter.setService(getCondensedExtendedService(parameters));
+        seriesParameter.setOffering(getCondensedExtendedOffering(series.getProcedure(), parameters));
+        seriesParameter.setProcedure(getCondensedExtendedProcedure(series.getProcedure(), parameters));
+        seriesParameter.setPhenomenon(getCondensedExtendedPhenomenon(series.getPhenomenon(), parameters));
+        seriesParameter.setFeature(getCondensedExtendedFeature(series.getFeature(), parameters));
+        seriesParameter.setCategory(getCondensedExtendedCategory(series.getCategory(), parameters));
+        seriesParameter.setPlatform(getCondensedPlatform(series.getPlatform(), parameters));
+        return seriesParameter;
+    }
+
+    protected org.n52.io.response.v1.ServiceOutput getCondensedService(DbQuery parameters ) throws DataAccessException {
+        String serviceId = serviceRepository.getServiceId();
+        org.n52.io.response.v1.ServiceOutput instance = serviceRepository.getCondensedInstance(serviceId, parameters);
+        org.n52.io.response.v1.ServiceOutput serviceOutput = new org.n52.io.response.v1.ServiceOutput();
+        serviceOutput.setLabel(instance.getLabel());
+        serviceOutput.setId(instance.getId());
+        return serviceOutput;
+    }
+
+    private org.n52.io.response.ServiceOutput getCondensedExtendedService(DbQuery parameters) {
+        String serviceId = serviceRepository.getServiceId();
+        org.n52.io.response.v1.ServiceOutput instance = serviceRepository.getCondensedInstance(serviceId, parameters);
+        org.n52.io.response.v1.ServiceOutput serviceOutput = new org.n52.io.response.v1.ServiceOutput();
+        serviceOutput.setLabel(instance.getLabel());
+        serviceOutput.setId(instance.getId());
+        serviceOutput.setHref(urHelper.getServicesHrefBaseUrl(parameters.getHrefBase()) + "/" + instance.getId());
+        return serviceOutput;
+    }
+
+    protected ParameterOutput getCondensedPhenomenon(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new PhenomenonOutput(), entity, parameters);
+    }
+
+    protected ParameterOutput getCondensedExtendedPhenomenon(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new PhenomenonOutput(), entity, parameters, urHelper.getPhenomenaHrefBaseUrl(parameters.getHrefBase()));
+    }
+
+    protected ParameterOutput getCondensedOffering(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new OfferingOutput(), entity, parameters);
+    }
+
+    protected ParameterOutput getCondensedExtendedOffering(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new OfferingOutput(), entity, parameters, urHelper.getOfferingsHrefBaseUrl(parameters.getHrefBase()));
+    }
+
+    private ParameterOutput createCondensed(ParameterOutput outputvalue, DescribableEntity entity, DbQuery parameters) {
+        outputvalue.setLabel(getLabelFrom(entity, parameters.getLocale()));
+        outputvalue.setId(Long.toString(entity.getPkid()));
+        return outputvalue;
+    }
+
+    private ParameterOutput createCondensed(ParameterOutput outputvalue, DescribableEntity entity, DbQuery parameters, String hrefBase) {
+        createCondensed(outputvalue, entity, parameters);
+        outputvalue.setHref(hrefBase + "/" + outputvalue.getId());
+        return outputvalue;
+    }
+
+    protected ParameterOutput getCondensedProcedure(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new ProcedureOutput(), entity, parameters);
+    }
+
+    protected ParameterOutput getCondensedExtendedProcedure(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new ProcedureOutput(), entity, parameters, urHelper.getProceduresHrefBaseUrl(parameters.getHrefBase()));
+    }
+
+    protected ParameterOutput getCondensedFeature(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new FeatureOutput(), entity, parameters);
+    }
+
+    protected ParameterOutput getCondensedExtendedFeature(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new FeatureOutput(), entity, parameters, urHelper.getFeaturesHrefBaseUrl(parameters.getHrefBase()));
+    }
+
+    protected ParameterOutput getCondensedCategory(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new CategoryOutput(), entity, parameters);
+    }
+
+    protected ParameterOutput getCondensedExtendedCategory(DescribableEntity entity, DbQuery parameters) {
+        return createCondensed(new CategoryOutput(), entity, parameters, urHelper.getCategoriesHrefBaseUrl(parameters.getHrefBase()));
+    }
+
+    protected ParameterOutput getCondensedPlatform(PlatformEntity entity, DbQuery parameters) {
+        return createCondensed(new PlatformOutput(entity.getPlatformType()), entity, parameters, urHelper.getPlatformsHrefBaseUrl(parameters.getHrefBase()));
+    }
+
 
 }
