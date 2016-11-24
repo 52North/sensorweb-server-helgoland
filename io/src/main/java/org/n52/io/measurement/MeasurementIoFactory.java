02443a0272f13107198e4cea2c11130976b35ef0
Henning Bredel <bredel@52north.org>
2016-11-24 15:35:10 +0100

    apply observation interval feature to CSV io handler
    this feature should close #157

--- io/src/main/java/org/n52/io/measurement/MeasurementIoFactory.java
+++ io/src/main/java/org/n52/io/measurement/MeasurementIoFactory.java
@@ -29,20 +29,17 @@
 
 package org.n52.io.measurement;
 
-import static org.n52.io.MimeType.APPLICATION_PDF;
-import static org.n52.io.MimeType.IMAGE_PNG;
-import static org.n52.io.MimeType.TEXT_CSV;
-import static org.n52.series.spi.srv.GeneralizingMeasurementDataService.composeDataService;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import org.n52.io.IoFactory;
 import org.n52.io.IoHandler;
 import org.n52.io.IoProcessChain;
 import org.n52.io.MimeType;
+import static org.n52.io.MimeType.APPLICATION_PDF;
+import static org.n52.io.MimeType.IMAGE_PNG;
+import static org.n52.io.MimeType.TEXT_CSV;
 import org.n52.io.measurement.csv.MeasurementCsvIoHandler;
 import org.n52.io.measurement.format.FormatterFactory;
 import org.n52.io.measurement.img.ChartIoHandler;
@@ -54,6 +51,7 @@ import org.n52.io.response.dataset.measurement.MeasurementData;
 import org.n52.io.response.dataset.measurement.MeasurementDatasetOutput;
 import org.n52.io.response.dataset.measurement.MeasurementValue;
 import org.n52.series.spi.srv.DataService;
+import static org.n52.series.spi.srv.GeneralizingMeasurementDataService.composeDataService;
 
 public final class MeasurementIoFactory extends IoFactory<MeasurementData, MeasurementDatasetOutput, MeasurementValue> {
 
@@ -123,7 +121,7 @@ public final class MeasurementIoFactory extends IoFactory<MeasurementData, Measu
             MeasurementCsvIoHandler handler = new MeasurementCsvIoHandler(
                                                                           getRequestParameters(),
                                                                           createProcessChain(),
-                                                                          createContext());
+                                                                          getMetadatas());
             handler.setTokenSeparator(parameters.getOther("tokenSeparator"));
 
             boolean byteOderMark = Boolean.parseBoolean(parameters.getOther("bom"));
