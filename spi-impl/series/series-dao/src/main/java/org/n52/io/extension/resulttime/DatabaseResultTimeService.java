d5c93a48f943ea50571e5ae4cbec5b959d1f3c5b
Henning Bredel <bredel@52north.org>
2017-02-20 12:18:49 +0100

    get result times and add them to the extra infos

---
+++ spi-impl/series/series-dao/src/main/java/org/n52/io/extension/resulttime/DatabaseResultTimeService.java
@@ -0,0 +1,27 @@
+package org.n52.io.extension.resulttime;
+
+import java.util.Set;
+
+import org.n52.io.request.IoParameters;
+import org.n52.io.response.dataset.DatasetType;
+import org.n52.series.db.dao.DbQuery;
+import org.n52.series.spi.srv.ResultTimeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DatabaseResultTimeService implements ResultTimeService {
+    
+    static final Logger LOGGER = LoggerFactory.getLogger(DatabaseResultTimeService.class);
+
+    private final ResultTimeRepository repository;
+    
+    public DatabaseResultTimeService(ResultTimeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Set<String> getResultTimeList(IoParameters parameters, String timeseriesId) {
+        return repository.getExtras(DatasetType.extractId(timeseriesId), DbQuery.createFrom(parameters));
+    }
+
+}
