195dcbb9fb79897178445625b1954421b003d21f
Henning Bredel <bredel@52north.org>
2017-03-04 12:22:26 +0100

    add parameters also on other dataset types

--- spi-impl/series/series-dao/src/main/java/org/n52/series/db/da/TextDataRepository.java
+++ spi-impl/series/series-dao/src/main/java/org/n52/series/db/da/TextDataRepository.java
@@ -151,6 +151,7 @@ public class TextDataRepository extends AbstractDataRepository<TextData, TextDat
         if (query.isExpanded()) {
             addGeometry(observation, value);
             addValidTime(observation, value);
+            addParameters(observation, value);
         } else if (series.getPlatform().isMobile()) {
             addGeometry(observation, value);
         }
