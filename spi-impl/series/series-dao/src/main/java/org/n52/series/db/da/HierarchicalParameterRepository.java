a7c74d0deb8a6f66098f5860eb1918dcd73584ce
Jan Schulte <j.schulte@52north.org>
2017-03-06 16:14:29 +0100

    add check for not setted hierarchical parameter

--- spi-impl/series/series-dao/src/main/java/org/n52/series/db/da/HierarchicalParameterRepository.java
+++ spi-impl/series/series-dao/src/main/java/org/n52/series/db/da/HierarchicalParameterRepository.java
@@ -14,28 +14,32 @@ import org.n52.series.db.dao.DbQuery;
 public abstract class HierarchicalParameterRepository<E extends HierarchicalEntity<E>, O> extends SessionAwareRepository implements OutputAssembler<O> {
 
     // TODO introduce lambdas here
-
+    
     protected List<O> createExpanded(Collection<E> entities, DbQuery parameters) throws DataAccessException {
         Set<O> results = new HashSet<>();
-        for (E entity : entities) {
-            O result = createExpanded(entity, parameters);
-            results.add(result);
-            if (entity.hasParents()) {
-                results.addAll(createExpanded(entity.getParents(), parameters));
+        if (entities != null) {
+            for (E entity : entities) {
+                O result = createExpanded(entity, parameters);
+                results.add(result);
+                if (entity.hasParents()) {
+                    results.addAll(createExpanded(entity.getParents(), parameters));
+                }
             }
         }
         return new ArrayList<>(results);
-}
+    }
 
     protected abstract O createExpanded(E procedureEntity, DbQuery parameters) throws DataAccessException;
 
     protected List<O> createCondensed(Collection<E> entities, DbQuery parameters) {
         Set<O> results = new HashSet<>();
-        for (E entity : entities) {
-            O result = createCondensed(entity, parameters);
-            results.add(result);
-            if (entity.hasParents()) {
-                results.addAll(createCondensed(entity.getParents(), parameters));
+        if (entities != null) {
+            for (E entity : entities) {
+                O result = createCondensed(entity, parameters);
+                results.add(result);
+                if (entity.hasParents()) {
+                    results.addAll(createCondensed(entity.getParents(), parameters));
+                }
             }
         }
         return new ArrayList<>(results);
