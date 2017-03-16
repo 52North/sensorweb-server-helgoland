cb07d0ccd3178c3f80953808ef452e1762acf756
Henning Bredel <bredel@52north.org>
2017-03-16 12:34:43 +0100

    add missing point restriction when querying instance

--- spi-impl/series/series-dao/src/main/java/org/n52/series/db/da/StationRepository.java
+++ spi-impl/series/series-dao/src/main/java/org/n52/series/db/da/StationRepository.java
@@ -165,10 +165,10 @@ public class StationRepository extends SessionAwareRepository implements OutputA
         return createExpanded(result, parameters, session);
     }
 
-    FeatureEntity getFeatureEntity(String id, DbQuery parameters, Session session) throws DataAccessException, BadRequestException {
+    private FeatureEntity getFeatureEntity(String id, DbQuery parameters, Session session) throws DataAccessException, BadRequestException {
         parameters.setDatabaseAuthorityCode(getDatabaseSrid());
-        FeatureDao featureDao = createDao(session);
-        return featureDao.getInstance(parseId(id), parameters);
+        DbQuery query = addPointLocationOnlyRestriction(parameters);
+        return createDao(session).getInstance(parseId(id), query);
     }
 
     public StationOutput getCondensedInstance(String id, DbQuery parameters, Session session) throws DataAccessException {
