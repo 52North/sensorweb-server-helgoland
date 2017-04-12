0ef0c80ab0befaa7500e4c80d8300b2e55b11d21
Henning Bredel <bredel@52north.org>
2017-04-12 12:30:38 +0200

    move ALTER OWNER statements to end of file

--- timeseries-spi-impl/sos-series-dao/src/main/resources/sql/update_views.sh
+++ timeseries-spi-impl/sos-series-dao/src/main/resources/sql/update_views.sh
@@ -2,10 +2,16 @@
 
 OWNER=sos_user
 DATABASE=sos_uba_v44
-OUTPUT_FILE=update_views_out.sql
+OUTPUT_FILE=create_views_out.sql
 SCHEMA=public
 
 echo "Generate SQL ..."
+
+echo "
+
+-- update/creation script for UBA views within 52°North SOS v4.3.11-UBA-1.0.3
+
+" > $OUTPUT_FILE
 
 insertView () {
     VIEW_NAME=$1
@@ -17,8 +23,6 @@ insertView () {
     FILE_CONTENT=`cat $FILE | sed 's/^\xEF\xBB\xBF//'`
     echo "$FILE_CONTENT" >>  $OUTPUT_FILE
     echo "WITH DATA;
-    ALTER TABLE  $SCHEMA.$VIEW_NAME
-    OWNER TO $OWNER;
 
     " >> $OUTPUT_FILE
 }
@@ -30,6 +34,13 @@ do
     echo "DROP MATERIALIZED VIEW IF EXISTS $VIEW_NAME CASCADE;
     " >> $OUTPUT_FILE
     insertView $VIEW_NAME $CURRENT_FILE
+done
+
+for CURRENT_FILE in $views
+do
+    VIEW_NAME=`echo $CURRENT_FILE | sed 's/queries\/\(.*\).sql$/\1/'`
+    echo "ALTER TABLE  $SCHEMA.$VIEW_NAME
+    OWNER TO $OWNER;" >> $OUTPUT_FILE
 done
 
 echo "Update views in database $DATABASE"
