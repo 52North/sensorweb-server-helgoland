ff55967d644993ba115707fbfd178050be8627a2
Henning Bredel <bredel@52north.org>
2017-04-20 09:27:22 +0200

    deploy to gh-pages after success

---
+++ .travis/on_success.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+stop() {
+  echo $* >&2
+  exit 0
+}
+
+[ "${TRAVIS_SECURE_ENV_VARS}" == "true" ] \
+  || stop "no secure enviroment variables were provided"
+
+[ "${TRAVIS_JOB_NUMBER}" == "${TRAVIS_BUILD_NUMBER}.1" ] \
+  || stop "only the first build job will be deployed"
+
+[ "${TRAVIS_PULL_REQUEST}" == "false" ] \
+  || stop "no deployment for pull requests"
+
+github_name="Travis CI"
+github_mail="travis@travis-ci.org"
+branch="gh-pages"
+deploy_dir=$(mktemp -d)
+
+git config --global user.name "${github_name}"
+git config --global user.email "${github_mail}"
+git clone --quiet --depth 1 --branch "${branch}" https://github.com/${TRAVIS_REPO_SLUG}.git "${deploy_dir}"
+
+rm -rf "${deploy_dir:?}/${TRAVIS_BRANCH}"
+cp -rv docs "${deploy_dir}/${TRAVIS_BRANCH}"
+
+pushd "${deploy_dir}"
+git add --ignore-removal .
+git add --update :/
+git commit -m "Updating ${TRAVIS_BRANCH} on ${branch} to ${TRAVIS_COMMIT}"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${branch}" >/dev/null 2>&1
+popd
