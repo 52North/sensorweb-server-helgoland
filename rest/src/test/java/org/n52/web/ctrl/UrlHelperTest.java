2f8d464e4cd4e165317934a3d1c6549f3c774f7d
Henning Bredel <bredel@52north.org>
2017-04-24 12:35:34 +0200

    remove constant v1 as API is backwards compatible

--- rest/src/test/java/org/n52/web/ctrl/UrlHelperTest.java
+++ rest/src/test/java/org/n52/web/ctrl/UrlHelperTest.java
@@ -37,33 +37,20 @@ public class UrlHelperTest {
 
     @Test
     public void when_constructBackwardsCompatibleUrl_then_relativeLink() {
-        String actual = new UrlHelper().constructHref(null, "/v1/procedures");
+        String actual = new UrlHelper().constructHref(null, "/procedures");
         assertThat(actual, is("./procedures"));
     }
 
     @Test
     public void when_constructUrlWithNonTrailingSlashBaseUrl_then_fullLink() {
-        String actual = new UrlHelper().constructHref("http://localhost:8080/foo/bar/v1", "/v1/procedures");
-        assertThat(actual, is("http://localhost:8080/foo/bar/v1/procedures"));
-    }
-
-    @Test
-    public void when_constructUrlFromVersionLessBaseUrl_then_fullLink() {
-        String actual = new UrlHelper().constructHref("http://localhost:8080/foo/bar/", "/v1/procedures");
-        assertThat(actual, is("http://localhost:8080/foo/bar/v1/procedures"));
+        String actual = new UrlHelper().constructHref("http://localhost:8080/foo/bar", "/procedures");
+        assertThat(actual, is("http://localhost:8080/foo/bar/procedures"));
     }
 
     @Test
     public void when_constructUrlFromBaseUrl_then_fullLink() {
-        String actual = new UrlHelper().constructHref("http://localhost:8080/foo/bar/v1/", "/v1/procedures");
-        assertThat(actual, is("http://localhost:8080/foo/bar/v1/procedures"));
+        String actual = new UrlHelper().constructHref("http://localhost:8080/foo/bar/", "/procedures");
+        assertThat(actual, is("http://localhost:8080/foo/bar/procedures"));
     }
-
-    @Test
-    public void when_constructUrlFromDeeperBaseUrl_then_fullLink() {
-        String actual = new UrlHelper().constructHref("http://localhost:8080/foo/bar/v1/somewhere", "/v1/procedures");
-        assertThat(actual, is("http://localhost:8080/foo/bar/v1/procedures"));
-    }
-
 
 }
