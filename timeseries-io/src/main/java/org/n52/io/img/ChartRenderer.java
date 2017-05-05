6a2c5f6a04225cf610df5e8a8200925f59b85277
Henning Bredel <bredel@52north.org>
2017-05-05 15:43:52 +0200

    use timezone ID instead of shortname

--- timeseries-io/src/main/java/org/n52/io/img/ChartRenderer.java
+++ timeseries-io/src/main/java/org/n52/io/img/ChartRenderer.java
@@ -68,7 +68,6 @@ import org.jfree.ui.HorizontalAlignment;
 import org.jfree.ui.RectangleEdge;
 import org.jfree.ui.RectangleInsets;
 import org.jfree.ui.VerticalAlignment;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Interval;
 import org.n52.io.I18N;
@@ -180,13 +179,7 @@ public abstract class ChartRenderer implements IoHandler {
     }
 
     private XYPlot createChart(RenderingContext context) {
-        String timespan = getTimespan();
-        DateTime end = timespan != null
-                    ? new DateTime(timespan.split("/")[1])
-                    : new DateTime();
-        
-        // String zoneName = getTimezone().getID();
-        String zoneName = getTimezone().getShortName(end.getMillis(), i18n.getLocale());
+        String zoneName = getTimezone().getID();
         StringBuilder domainAxisLabel = new StringBuilder(i18n.get("time"));
         domainAxisLabel.append(" (")
                        .append(zoneName)
