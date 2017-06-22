f168ab679e7aa1cf176cc1d6b0176decd1ae5ce9
Henning Bredel <h.bredel@52north.org>
2017-06-22 14:06:21 +0200

    fix issue when example block contains p lement before toggle content

--- web-resources/src/main/docs/assets/javascripts/52n/example-toggle-details.js
+++ web-resources/src/main/docs/assets/javascripts/52n/example-toggle-details.js
@@ -9,21 +9,13 @@
       hideContent: function(content, button) {
           content.addClass('hide');
           button.setAttribute('title', 'Show content');
-      },
-      toggleContent: function(button) {
-          var toggle_block = $(button).next('div');
-          if (button.classList.contains('active')) {
-              this.showContent(toggle_block, button);
-          } else {
-              this.hideContent(toggle_block, button);
-          }
-      },
+      }
   };
   
   $(function() {
     $('.n52-example-toggler.btn').each(function(i, element) {
         var button = element;
-        var toggle_block = $(button).next('div');
+        var toggle_block = $(button).nextAll('div');
         if (button.classList.contains('active')) {
             N52ContentToggler.showContent(toggle_block, button);
         } else {
@@ -32,11 +24,10 @@
     });
     $('.n52-example-toggler.btn').bind('click', function (event) {
         var button = event.currentTarget;
-        var toggle_block = $(button).next('div')
+        var toggle_block = $(button).nextAll('div')
         if (toggle_block.hasClass('hide')) {
             N52ContentToggler.showContent(toggle_block, button);
-        }
-        else {
+        } else {
             N52ContentToggler.hideContent(toggle_block, button);
         }
       });
