5d3dc1761901f5f33f4cbbb9af8ee1415fceb8cf
Henning Bredel <h.bredel@52north.org>
2017-08-17 15:00:05 +0200

    fix array

--- .travis/deploy-docs.sh
+++ .travis/deploy-docs.sh
@@ -6,22 +6,22 @@ stop() {
 }
 
 DOCUMENTATION_BRANCHES=(
-  "documentation/docs-via-swagger-jekyll"
-  "develop",
-  "master",
+  "documentation/docs-via-swagger-jekyll" \
+  "develop" \
+  "master" \
   "versions/1.10.x"
 )
 
 deploy_on_ghpages() {
   var="$1"
   for item in ${DOCUMENTATION_BRANCHES[@]} ; do
-      [[ $var == "$item" ]] && return 0
+      [ "$var" == "$item" ] && return 0
   done
   return 1
 }
 
-#[ !$(deploy_on_ghpages "${TRAVIS_BRANCH}") ] \
-#  || stop "do not deploy docs of ${TRAVIS_BRANCH}"
+[ !$(deploy_on_ghpages "${TRAVIS_BRANCH}") ] \
+  || stop "do not deploy docs of ${TRAVIS_BRANCH}"
 
 [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ] \
   || stop "no secure enviroment variables were provided"
