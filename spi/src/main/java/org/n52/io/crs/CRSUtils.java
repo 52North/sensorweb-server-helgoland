c51ccbfd4e8ef28d3be140f36b557daed8480521
Henning Bredel <h.bredel@52north.org>
2018-01-18 12:31:09 +0100

    some formatting

--- spi/src/main/java/org/n52/io/crs/CRSUtils.java
+++ spi/src/main/java/org/n52/io/crs/CRSUtils.java
@@ -70,7 +70,7 @@ public final class CRSUtils {
     private static CoordinateReferenceSystem internCrs;
 
     protected CRSAuthorityFactory crsFactory;
-    
+
     /**
      * use static constructors to create an instance.
      *
