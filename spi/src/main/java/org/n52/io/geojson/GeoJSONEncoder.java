89b6517c8475b024f5de8ba108e4d3dde57289eb
Carsten Hollmann <c.hollmann@52north.org>
2020-07-09 09:45:03 +0200

    add missing imports

--- spi/src/main/java/org/n52/io/geojson/GeoJSONEncoder.java
+++ spi/src/main/java/org/n52/io/geojson/GeoJSONEncoder.java
@@ -38,7 +38,9 @@ import org.locationtech.jts.geom.MultiPoint;
 import org.locationtech.jts.geom.MultiPolygon;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.impl.CoordinateArraySequence;
 import org.n52.io.crs.CRSUtils;
+import org.n52.shetland.util.JTSHelper;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
